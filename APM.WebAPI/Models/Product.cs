394748f30355a8a9c98801bfb5e7317d6e98ac6a
Allen Nielson <aknielson@hotmail.com>
2016-05-26 12:30:49 -0700

    Add project files.

---
+++ APM.WebAPI/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace APM.WebAPI.Models
+{
+    public class Product
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ProductCode { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+        //"productId": 1,
+        //  "productName": "Leaf Rake",
+        //  "productCode": "GDN-0011",
+        //  "releaseDate": "March 19, 2009",
+        //  "description": "Leaf rake with 48-inch wooden handle.",
+        //  "price": 19.95
+    }
+}
